68241230dffa415acd7926afd897f775b16879c0
Amir <amir_dawd@hotmail.com>
2019-04-03 11:43:42 +0200

    Compass and Accelerometer App.

--- app/src/main/java/com/example/firstapp/Accelerometer.java
+++ app/src/main/java/com/example/firstapp/Accelerometer.java
@@ -1,11 +1,13 @@
 package com.example.firstapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.SensorEventListener;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
+import android.widget.TextView;
 
 public class Accelerometer extends AppCompatActivity implements SensorEventListener {
     private SensorManager senSensorManager;
@@ -13,6 +15,7 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
     private long lastUpdate = 0;
     private float last_x, last_y, last_z;
     private static final int SHAKE_THRESHOLD = 600;
+    private TextView textX, textY, textZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,11 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+        textX = (TextView)findViewById(R.id.xvalue);
+        textY = (TextView)findViewById(R.id.yvalue);
+        textZ= (TextView)findViewById(R.id.zvalue);
     }
-    @override
+    @Override
     protected void onPause() {
         super.onPause();
         senSensorManager.unregisterListener(this);
@@ -34,7 +40,7 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    Override
+   @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
 
@@ -44,22 +50,15 @@ public class Accelerometer extends AppCompatActivity implements SensorEventListe
                 float z = event.values[2];
             long curTime = System.currentTimeMillis();
 
-            if ((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
-                lastUpdate = curTime;
-                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+            textX.setText("X-value: "+ x);
 
-                if (speed > SHAKE_THRESHOLD) {
+            textY.setText("Y-value: " + y);
 
-                }
+            textZ.setText("Z-value: " + z);
 
-                last_x = x;
-                last_y = y;
-                last_z = z;
-            }
             }
         }
-    }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
